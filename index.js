754ebb5de7c739c824648140c128c8e201b491b6
Stella <stellaclough@gmail.com>
2024-12-11 12:20:40 +0100

    Added show less toggle

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+
+//Show more button
+
 let showMoreButton = document.querySelector('.showmore');
 showMoreButton.addEventListener('click', function() {
-showMoreButton.classList.add('showingmore');
+    showMoreButton.classList.toggle('showingmore');
+    // Change the text on the button to 'Show more' when it's
+    //'Show less', and to 'Show less' when it's 'Show more'
+    if (showMoreButton.textContent == 'Show more') {
+        showMoreButton.textContent = 'Show less';
+    } else {
+        showMoreButton.textContent = 'Show more';
+    }
 })
 
-let hamburgerButton = document.querySelector('.sidenav');
-hamburgerButton.addEventListener('click', function() {
-hamburgerButton.classList.add('openNav', 'backgroundblur');
-})
+
+//let showlessButton = document.querySelector('.showingmore');
+//showlessButton.addEventListener('click', function() {
+//showlessButton.classList.remove('showingmore');
+//showlessButton.classList.add('showingless');
+//})
+
+//search field
+
+let inputElement = document.querySelector('input');
+
+inputElement.addEventListener('invalid', function(ev) {
+
+  inputElement.setCustomValidity('fill this field in to search');
+
+});
 
